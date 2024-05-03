6a5e60682bcc9b3d55cfa799ba9534b80a660053
Nicks-Souza <nicolas.souza990@gmail.com>
2024-05-03 19:47:32 -0300

    modificando o script das classes

--- src/pages/scriptClasse.js
+++ src/pages/scriptClasse.js
@@ -8,35 +8,34 @@ function applyEffect() {
     setTimeout(function() {
         var cardFront = document.querySelector('.card_front');
         const aboutClasse=document.querySelector('#about_classe');
-
         
-        cardFront.classList.remove("hidden");
-        // Define a posição como absoluta para ajustar com base no elemento pai
-        cardFront.style.position = 'absolute'; 
-        // Coloca o elemento na metade da largura do contêiner pai
-        cardFront.style.left = '100%';
-        // Move o elemento para a direita
-        cardFront.style.transform = 'translateX(20%)';
-        // Adiciona uma transição ao deslocamento
-        cardFront.style.transition = 'left 0.5s, transform 0.5s';
-
-        aboutClasse.classList.remove("hidden");
-        aboutClasse.classList.add("about_classe");
-        aboutClasse.style.transform = 'translateX(150%)'; // Ajuste aqui para se deslocar junto
-        aboutClasse.style.transition = 'left 0.5s, transform 0.5s';
-        //Aqui se encontra o recurso a ser aprimorado
-        cartaoContent.style.transform = 'rotateY(0deg)';
+        if (cardFront.classList.contains("hidden")) {
+            cardFront.classList.remove("hidden");
+            // Define a posição como absoluta para ajustar com base no elemento pai
+            cardFront.style.position = 'absolute'; 
+            // Coloca o elemento na metade da largura do contêiner pai
+            cardFront.style.left = '100%';
+            // Move o elemento para a direita
+            cardFront.style.transform = 'translateX(20%)';
+            // Adiciona uma transição ao deslocamento
+            cardFront.style.transition = 'left 0.5s, transform 0.5s';
+            //Aqui se encontra o recurso a ser aprimorado
+            cartaoContent.style.transform = 'rotateY(0deg)';
+        }else{
+            cardFront.classList.add("hidden");
+        }
+        if(aboutClasse.classList.contains("hidden")){
+            aboutClasse.classList.remove("hidden");
+            aboutClasse.style.transform = 'translateX(150%)'; // Ajuste aqui para se deslocar junto
+            aboutClasse.style.transition = 'left 0.5s, transform 0.5s';
+        }else{
+            aboutClasse.classList.add("hidden");
+        }
+        
         
     }, 500); // Tempo correspondente ao tempo de transição do card_back
 }
-  
-    function revertEffect() {
-      var cartaoContent = document.querySelector('.cartao_content');
-      cartaoContent.style.transform = 'rotateY(0deg)';
 
-      
-      cardFront.style.left = '0';
-    }
 
     function redirecionar() {
         window.location.href = "./ficha.html";
