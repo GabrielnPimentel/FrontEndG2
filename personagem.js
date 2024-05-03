6419bb81d5b1f37055c74ad045d33178d2990ae7
MatheusARodrigues <matheusadrianor7@gmail.com>
2024-05-03 13:50:52 -0300

    Fix: retirando a musica.

--- personagem.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var audio = document.getElementById("hades");
-    audio.volume = 0.1;
-});
-
-var audio = document.getElementById("hades");
-audio.volume = 0.1;
