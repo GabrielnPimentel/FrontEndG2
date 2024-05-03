7292396ca0894eba58cfea6162f727a7305a667b
GabrielnPimentel <gabrielnpimentel@hotmail.com>
2024-05-03 17:37:47 -0300

    feat: criação das páginas individuais das classes

--- src/pages/scriptClasse.js
+++ src/pages/scriptClasse.js
@@ -4,14 +4,13 @@ function applyEffect() {
     // Gira o cartaoContent   
     cartaoContent.style.transform = 'rotateY(180deg)';
     cartaoContent.style.transition = 'transform 0.5s';   
-    
+
     setTimeout(function() {
         var cardFront = document.querySelector('.card_front');
-        const cardDesc=document.querySelector('.descricao');
-        const aboutPersona=document.querySelector('.about_personagem');
+        const aboutClasse=document.querySelector('#about_classe');
 
-        cardFront.classList.remove("hidden");
         
+        cardFront.classList.remove("hidden");
         // Define a posição como absoluta para ajustar com base no elemento pai
         cardFront.style.position = 'absolute'; 
         // Coloca o elemento na metade da largura do contêiner pai
@@ -21,13 +20,12 @@ function applyEffect() {
         // Adiciona uma transição ao deslocamento
         cardFront.style.transition = 'left 0.5s, transform 0.5s';
 
-        cardDesc.classList.remove("hidden");
-        cardDesc.style.transform = 'translateX(150%)'; // Ajuste aqui para se deslocar junto
-        cardDesc.style.transition = 'left 0.5s, transform 0.5s';
-
-        aboutPersona.classList.remove('hidden');
-        aboutPersona.style.transform='translateX(20%)'; // Ajuste aqui para se deslocar junto
-        aboutPersona.style.transition = 'left 0.5s, transform 0.5s';
+        aboutClasse.classList.remove("hidden");
+        aboutClasse.classList.add("about_classe");
+        aboutClasse.style.transform = 'translateX(150%)'; // Ajuste aqui para se deslocar junto
+        aboutClasse.style.transition = 'left 0.5s, transform 0.5s';
+        //Aqui se encontra o recurso a ser aprimorado
+        cartaoContent.style.transform = 'rotateY(0deg)';
         
     }, 500); // Tempo correspondente ao tempo de transição do card_back
 }
@@ -35,7 +33,13 @@ function applyEffect() {
     function revertEffect() {
       var cartaoContent = document.querySelector('.cartao_content');
       cartaoContent.style.transform = 'rotateY(0deg)';
+
+      
       cardFront.style.left = '0';
+    }
+
+    function redirecionar() {
+        window.location.href = "index.html";
     }
   
   
